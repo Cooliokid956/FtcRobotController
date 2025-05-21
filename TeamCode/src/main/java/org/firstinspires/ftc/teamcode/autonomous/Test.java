fdf377a80afb4ca4de56338ede40a9f73fdd063d
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2025-05-21 13:42:41 -0500

    He is writing his own Autonomous OpMode
    Guided

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Test.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
 
+@Autonomous
 public class Test extends OpMode {
     MecanumDrive drive = new MecanumDrive();
 
@@ -16,7 +18,7 @@ public class Test extends OpMode {
 
     @Override
     public void loop() {
-
+        pad.left_stick_y = 1.f;
 
         drive.drive(pad, telemetry);
     }
