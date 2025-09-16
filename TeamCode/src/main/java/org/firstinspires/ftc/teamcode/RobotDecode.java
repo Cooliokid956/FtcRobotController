b2e36b410a87994c0bdb9199d7739cd91a10b45a
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2025-09-16 14:06:18 -0500

    gut new OpMode
    the SuperArm is useless for this year's game (and dismantled)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDecode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDecode.java
@@ -11,18 +11,9 @@ public class RobotDecode extends OpMode {
     // Drive
     MecanumDrive drive  = new MecanumDrive();
 
-    // Components
-    SuperArm arm = new SuperArm();
-
-    void setPitchAndLength(int p, int l) {
-        arm.armPos = p;
-        arm.slidePos = l;
-    }
-
     @Override
     public void init() {
         drive.config.init(hardwareMap);
-        arm  .config.init(hardwareMap);
     }
 
     @Override
@@ -33,15 +24,7 @@ public class RobotDecode extends OpMode {
     @Override
     public void loop() {
         drive.drive(gamepad1, telemetry);
-        arm.moveArm((int)((gamepad1.right_trigger - gamepad1.left_trigger) * 15.f));
         telemetry.addData("lt", gamepad1.left_trigger);
         telemetry.addData("rt", gamepad1.right_trigger);
-        arm.update(gamepad1);
-
-        if (gamepad1.dpad_up  ) setPitchAndLength(3700,1600);
-        if (gamepad1.dpad_down) setPitchAndLength(0,0);
-
-        arm.config.slide.setPower(gamepad1.left_bumper ? .55 : .2);
-        telemetry.addData("pitch", arm.config.arm.getCurrentPosition());
     }
 }
