e2c9e7a972b90657539668164a59b3f879d637ef
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2026-01-13 13:38:18 -0600

    12393: Cylinder controls

--- Team12393/src/main/java/org/firstinspires/ftc/teamcode/Drive_Train.java
+++ Team12393/src/main/java/org/firstinspires/ftc/teamcode/Drive_Train.java
@@ -2,38 +2,36 @@ package org.firstinspires.ftc.teamcode;
 import static java.lang.Math.*;
 
 
-import android.hardware.Sensor;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 @TeleOp
 public class Drive_Train extends OpMode {
-    private DcMotorEx frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, reversed_outtake_Motor, outtake_Motor, intake_Motor, rotateMotor;
+    private DcMotorEx frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, reversed_outtake_Motor, outtake_Motor, intake_Motor;
     private CRServo Is, rs, trigger;
+
+    DcMotorEx cylinder;
+    int
+            cylOffsetTicks = 48,
+            cylChamberTicks = 96,
+            cylTicks;
+    boolean
+            cylShoot, // (In, Out)
+            lTrigDown,
+            rTrigDown;
 
     @Override
     public void init() {
-        //Webcam -> webcam
-        //ControlHub -> Motors -> 1:Hex 40:1 OuttakeMotor1, 2:GoBilda 5202/3/4? frm 3: GoBilda 5202/3/4? brm
-        //ExpansionHub -> Motors -> 0: GoBilda 5202/3/4? blm 1: GoBilda 5202/3/4? flm 2: Hex Motor 40:1 IntakeMotor 3: Go bilda 5202/3/4 outtakeMotor2
+        // Webcam -> webcam
+        // ControlHub -> Motors -> 1:Hex 40:1 OuttakeMotor1, 2:GoBilda 5202/3/4? frm 3: GoBilda 5202/3/4? brm
+        // ExpansionHub -> Motors -> 0: GoBilda 5202/3/4? blm 1: GoBilda 5202/3/4? flm 2: Hex Motor 40:1 IntakeMotor 3: Go bilda 5202/3/4 outtakeMotor2
         reversed_outtake_Motor = hardwareMap.get(DcMotorEx.class, "OuttakeMotor2");
         outtake_Motor = hardwareMap.get(DcMotorEx.class, "OuttakeMotor1");
         intake_Motor = hardwareMap.get(DcMotorEx.class, "IntakeMotor" );
-        rotateMotor = hardwareMap.get(DcMotorEx.class,"rotateMotor");
         Is = hardwareMap.get(CRServo.class, "Is");
         rs = hardwareMap.get(CRServo.class, "rs");
         trigger = hardwareMap.get(CRServo.class,"trigger");
@@ -57,46 +55,51 @@ public class Drive_Train extends OpMode {
         backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        rotateMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        rotateMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        cylinder = hardwareMap.get(DcMotorEx.class, "rotateMotor");
+        cylinder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        cylinder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        cylinder.setPower(.1);
+//        cylinder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        //Powers Both servos to push upwards when right trigger in held otherwise stops in place
+        // Powers Both servos to push upwards when right trigger in held otherwise stops in place
         /*
         if (gamepad1.right_trigger >= 0.2) { Is.setPower(-1); rs.setPower(1); } else { Is.setPower(0); rs.setPower(0); }
         */
-        //Powers Both servos to push downwards when right trigger in held otherwise stops in place
+        // Powers Both servos to push downwards when right trigger in held otherwise stops in place
         /*
         if (gamepad1.left_trigger >= 0.2) { Is.setPower(1); rs.setPower(-1); } else { Is.setPower(0); rs.setPower(0); }
         */
 
 
-        //Powers trigger servo forward (left bumper) or back (button b)
-        //if (gamepad1.left_bumper) trigger.setPower(1); else if (gamepad1.b) trigger.setPower(-1); else trigger.setPower(0);
+        // Powers trigger servo forward (left bumper) or back (button b)
+        // if (gamepad1.left_bumper) trigger.setPower(1); else if (gamepad1.b) trigger.setPower(-1); else trigger.setPower(0);
 
         trigger.setPower(gamepad1.left_bumper ? 1 : (gamepad1.b ? -1 : 0));
 
-        //Rotate motor code for it to go into the set position everytime you press the right trigger
-        if (gamepad1.right_trigger >= 0.2) {
-            rotateMotor.setTargetPosition(48);
-            rotateMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rotateMotor.setPower(.1);
-        }
+        // cylinder control code
+        // L Trigger - Toggle mode
+        // R Trigger - Rotate cylinder
+        // R Bumper - Action (Intake/Outtake)
+        boolean action = gamepad1.right_bumper;
+        boolean lTrig = gamepad1.left_trigger > 0.2;
+        boolean rTrig = gamepad1.right_trigger > 0.2;
 
-        if(rotateMotor.getTargetPosition() == rotateMotor.getCurrentPosition()) {
-            rotateMotor.setPower(0);
-            rotateMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-        //Has the Outtake motors power on and start spinning to shoot when the button x is held down
-        reversed_outtake_Motor.setPower(gamepad1.x ? .8 : 0);
-        outtake_Motor.setPower(gamepad1.x ? 1 : 0);
+        cylShoot = lTrig; // modifier
+//        if (lTrig && !lTrigDown) cylShoot = !cylShoot;
+//        lTrigDown = lTrig; // toggle
 
-        //Has the intake motor start spinning inwards when the right bumper is help down
-        intake_Motor.setPower(gamepad1.right_bumper ? -.8 : 0);
-        //Rotates the motor
+        if (rTrig && !rTrigDown) cylTicks += cylChamberTicks;
+        rTrigDown = rTrig;
 
+        cylinder.setTargetPosition(cylTicks + (cylShoot ? cylOffsetTicks : 0));
+
+        if (cylShoot) { // shoot mode
+            reversed_outtake_Motor.setPower(action ? .8 : 0);
+            outtake_Motor.setPower(action ? 1 : 0);
+        } else intake_Motor.setPower(action ? -.8 : 0); // intake mode
 
         /*
         Code to calculate the power necessary to give each motor in the strafe mechanism
@@ -124,9 +127,9 @@ public class Drive_Train extends OpMode {
         telemetry.addData("backRightPower", backRightPower);
         telemetry.addData("Left Bumper Activated",gamepad1.left_bumper);
 
-        telemetry.addData("TargetPosition", rotateMotor.getTargetPosition());
-        telemetry.addData("Current Position", rotateMotor.getCurrentPosition());
-        telemetry.addData("Motor Mode", rotateMotor.getMode());
+        telemetry.addData("TargetPosition", cylinder.getTargetPosition());
+        telemetry.addData("Current Position", cylinder.getCurrentPosition());
+        telemetry.addData("Motor Mode", cylinder.getMode());
         telemetry.update();
 //        reversed_outtake_Motor.setPower(gamepad1.x ? .8 : gamepad1.a ? -1 : 0);
 //        outtake_Motor.setPower(gamepad1.x ? 1 : gamepad1.a ? -1 : 0);
