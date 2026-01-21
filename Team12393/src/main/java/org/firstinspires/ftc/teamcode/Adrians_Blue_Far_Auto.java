e8e76c5ba248e0184789787f4ef5396cdc426283
doomster1278 <233189732+doomster1278@users.noreply.github.com>
2026-01-21 11:51:36 -0600

    12393: Blue far auto work not done
    Work in progress

--- Team12393/src/main/java/org/firstinspires/ftc/teamcode/Adrians_Blue_Far_Auto.java
+++ Team12393/src/main/java/org/firstinspires/ftc/teamcode/Adrians_Blue_Far_Auto.java
@@ -1,23 +1,46 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+import com.qualcomm.robotcore.hardware.Servo;
+@Autonomous
 public class Adrians_Blue_Far_Auto extends LinearOpMode {
     private DcMotorEx frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, reversed_outtake_Motor, outtake_Motor, intake_Motor;
-    private CRServo Is;
+    //     private CRServo Is, rs;, trigger;
+    private Servo trigger;
+
+    DcMotorEx cylinder;
+
+    int backLeftPosition = 0;
+    int backRightPosition = 0;
+    int frontLeftPosition = 0;
+    int frontRightPosition = 0;
+
+    int
+            cylOffsetTicks = 48,
+            cylChamberTicks = 96,
+            cylTicks;
+    boolean
+            cylShoot, // (In, Out)
+            lTrigDown,
+            rTrigDown;
 
     @Override
     public void runOpMode() {
+        // Webcam -> webcam
+        // ControlHub -> Motors -> 1:Hex 40:1 OuttakeMotor1, 2:GoBilda 5202/3/4? frm 3: GoBilda 5202/3/4? brm
+        // ExpansionHub -> Motors -> 0: GoBilda 5202/3/4? blm 1: GoBilda 5202/3/4? flm 2: Hex Motor 40:1 IntakeMotor 3: Go bilda 5202/3/4 outtakeMotor2
         reversed_outtake_Motor = hardwareMap.get(DcMotorEx.class, "OuttakeMotor2");
         outtake_Motor = hardwareMap.get(DcMotorEx.class, "OuttakeMotor1");
-        intake_Motor = hardwareMap.get(DcMotorEx.class, "IntakeMotor");
-        Is = hardwareMap.get(CRServo.class, "Is");
+        intake_Motor = hardwareMap.get(DcMotorEx.class, "IntakeMotor" );
+
+        trigger = hardwareMap.get(Servo.class,"trigger");
+        trigger.setDirection(Servo.Direction.REVERSE);
         outtake_Motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        intake_Motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         frontLeftMotor = hardwareMap.get(DcMotorEx.class, "flm");
         frontRightMotor = hardwareMap.get(DcMotorEx.class, "frm");
@@ -32,30 +55,47 @@ public class Adrians_Blue_Far_Auto extends LinearOpMode {
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setTargetPosition(0);
+        frontRightMotor.setTargetPosition(0);
+        backLeftMotor.setTargetPosition(0);
+        backRightMotor.setTargetPosition(0);
+
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
+
+        cylinder = hardwareMap.get(DcMotorEx.class, "rotateMotor");
+        cylinder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        cylinder.setTargetPosition(0);
+        cylinder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        cylinder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        cylinder.setPower(.5);
+        cylinder.setPositionPIDFCoefficients(20);
 
         //Auto starts here
         waitForStart();
-//        moveBackward(1000);
-//        pause(200);
-//        turnRight(1000);
-//        pause(200);
-//        rev(1500);
-//        launch(2500);
-//        turnLeft(1000);
-//        pause(200);
-//        moveBackward(1000);
-//        pause(200);
-//        turnLeft(1000);
-//        pause(200);
-        moveForward(1000); //Adjust to get to the top of the Triangle
+        trigger.setPosition(1);
+        moveBackward(5000); //Adjust to get to the top of the Triangle
+        turnLeft(20000);
         pause(500);
+        rev(5000);
+        launch();
+        loadNext();
 
 
 
+        goToIntake();
 
         sleep(1000);
         frontLeftMotor.setPower(0);
@@ -108,25 +148,21 @@ public class Adrians_Blue_Far_Auto extends LinearOpMode {
         );
     }
 
-    private void resetEncoders()
+    private void getToPosition()
     {
-        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        while(frontLeftPosition > frontLeftMotor.getCurrentPosition())
+        {
+            continue;
+        }
     }
+
     private void setTargetPositions(int fl, int fr, int bl, int br)
     {
-        frontLeftMotor.setTargetPosition(fl);
-        frontRightMotor.setTargetPosition(fr);
-        backLeftMotor.setTargetPosition(bl);
-        backRightMotor.setTargetPosition(br);
-        resetEncoders();
+        frontLeftMotor.setTargetPosition(frontLeftPosition += fl);
+        frontRightMotor.setTargetPosition(frontRightPosition += fr);
+        backLeftMotor.setTargetPosition(backLeftPosition += bl);
+        backRightMotor.setTargetPosition(backRightPosition += br);
+        getToPosition();
     }
     private void pause(int ms)
     {
@@ -134,7 +170,13 @@ public class Adrians_Blue_Far_Auto extends LinearOpMode {
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
         backRightMotor.setPower(0);
+
         sleep(ms);
+
+        frontLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
     }
     private void rev(int ms)
     {
@@ -142,9 +184,20 @@ public class Adrians_Blue_Far_Auto extends LinearOpMode {
         reversed_outtake_Motor.setPower(1);
         pause(ms);
     }
-    private void launch(int ms)
+    private void launch()
     {
-        Is.setPower(-1);
-        pause(ms);
+        trigger.setPosition(-1);
+        pause(2000);
+        trigger.setPosition(1);
+    }
+
+    private void loadNext()
+    {
+        cylinder.setTargetPosition(cylTicks + 96);
+    }
+
+    private void goToIntake()
+    {
+        cylinder.setTargetPosition(cylTicks + 48);
     }
 }
